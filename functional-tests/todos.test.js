8a5a08d9a8c035d88851f83f5a72c9e49656518b
therynamo <groetkentheryn@gmail.com>
2019-04-19 12:23:28 -0500

    Add Functional Test Shell

---
+++ functional-tests/todos.test.js
@@ -0,0 +1,13 @@
+describe('Todos', () => {
+  beforeAll(async () => {
+    await page.goto('http://localhost:3000');
+  });
+
+  it('should add a todo', async () => {});
+
+  it('should remove a todo', async () => {});
+
+  it('should allow you to view the details of a todo', async () => {});
+
+  it('should allow you to return to the list view of todos', async () => {});
+});

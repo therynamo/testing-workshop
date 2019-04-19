450f4c4a3386c2823a26be76e0381c434d1e9022
therynamo <groetkentheryn@gmail.com>
2019-04-19 10:53:01 -0500

    Shell Files

---
+++ src/actions/transforms/todosTransform.js
@@ -0,0 +1,22 @@
+/**
+ * Current Contract:
+ *
+ * [{
+ *    id: '134',
+ *    title: 'MY TITLE,
+ *    description: 'MY DESC',
+ * }]
+ *
+ */
+
+/**
+ * Expected Contract:
+ *
+ * {
+ *   '134': {
+ *     id: '134',
+ *     title: 'MY TITLE',
+ *     description: 'MY DESC',
+ *   }
+ * }
+ */
